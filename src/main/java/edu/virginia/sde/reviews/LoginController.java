d595623a2fb81b7eef79575cabb6a95fe25a2f9f
kevinlam0 <kevinclam02@gmail.com>
2023-11-18 15:31:56 -0500

    Added some comments for login

---
+++ src/main/java/edu/virginia/sde/reviews/LoginController.java
@@ -0,0 +1,13 @@
+package edu.virginia.sde.reviews;
+
+public class LoginController {
+    public void logInExistingAccount(String username, String password) {
+        // THERE NEEDS TO BE A TABLE FOR THE ACCOUNTS
+
+        /* IF THE USERNAME OR PASSWORD IS INCORRECT DO SOMETHING */
+        // USERNAME DOES NOT EXIST
+
+        // PASSWORD IS INCORRECT
+
+    }
+}
