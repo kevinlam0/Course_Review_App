285eaefae1c9392a902070101ca06167c1d0d827
kevinlam0 <kevinclam02@gmail.com>
2023-11-18 15:03:10 -0500

    Initialized the database driver

---
+++ src/main/java/edu/virginia/sde/reviews/DatabaseDriver.java
@@ -0,0 +1,22 @@
+package edu.virginia.sde.reviews;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseDriver {
+    private final String sqliteFile;
+    private Connection connection;
+    public DatabaseDriver(String sqliteFileName) { this.sqliteFile = sqliteFileName; }
+    public void connect() throws SQLException {
+        if (connection != null && !connection.isClosed()) {
+            throw new IllegalStateException("The connection is already opened.");
+        }
+        connection = DriverManager.getConnection("jdbc:sqlite:" + this.sqliteFile);
+        connection.createStatement().execute("PRAGMA foreign_keys = ON");
+        connection.setAutoCommit(false);
+    }
+    public void commit() throws SQLException { connection.commit(); }
+    public void rollback() throws SQLException { connection.rollback(); }
+    public void disconnect() throws SQLException { connection.close(); }
+}
