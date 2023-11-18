9ddd42b1174e1157612fcb6ad952d78129175375
kevinlam0 <kevinclam02@gmail.com>
2023-11-18 17:37:28 -0500

    Added method to check if a user exists

--- src/main/java/edu/virginia/sde/reviews/LoginDataDriver.java
+++ src/main/java/edu/virginia/sde/reviews/LoginDataDriver.java
@@ -1,6 +1,9 @@
 package edu.virginia.sde.reviews;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.*;
 
 public class LoginDataDriver extends DatabaseDriver{
     private final String createUserTableSQL = """
@@ -10,11 +13,37 @@ public class LoginDataDriver extends DatabaseDriver{
                 Password TEXT
             );
             """;
-    public LoginDataDriver(String sqliteFileName) {
-        super(sqliteFileName);
-    }
+    public LoginDataDriver(String sqliteFileName) { super(sqliteFileName); }
     public void createTable() throws SQLException {
         super.connection.prepareStatement(createUserTableSQL);
+    }
+    public Set<String> getAllUsers() throws SQLException {
+        if (super.connection.isClosed()) {
+            throw new IllegalStateException("The connection is not open. You cannot get the users.");
+        }
+        PreparedStatement statement = connection.prepareStatement("SELECT * FROM Users");
+        Set<String> users = new HashSet<>();
+        ResultSet resultSet = statement.executeQuery();
+        while (resultSet.next()) {
+            String user = resultSet.getString(1);
+            users.add(user);
+        }
+        statement.close();
+        return users;
+    }
+    public boolean doesUserExist(String username) throws SQLException {
+        if (connection.isClosed()) {
+            throw new IllegalStateException("Connection is not open");
+        }
+        PreparedStatement statement = connection.prepareStatement(
+                ("SELECT * FROM Users WHERE Username = ?"));
+        statement.setString(1, username);
+
+        ResultSet resultSet = statement.executeQuery();
+        return !isEmpty(resultSet);
+    }
+    private boolean isEmpty(ResultSet resultSet) throws SQLException {
+        return !resultSet.isBeforeFirst() && resultSet.getRow() == 0;
     }
 
 }
