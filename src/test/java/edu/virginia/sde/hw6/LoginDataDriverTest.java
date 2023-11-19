aa08b46d75724593b7f97a72668a2c4dc5b4cfba
kevinlam0 <kevinclam02@gmail.com>
2023-11-19 09:46:27 -0500

    Testing doesUserExists

--- src/test/java/edu/virginia/sde/hw6/LoginDataDriverTest.java
+++ src/test/java/edu/virginia/sde/hw6/LoginDataDriverTest.java
@@ -1,11 +1,20 @@
 package edu.virginia.sde.hw6;
 
 import edu.virginia.sde.reviews.LoginDataDriver;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestTemplate;
 
 import java.sql.SQLException;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class LoginDataDriverTest {
+    @BeforeEach
+    void setUp() throws SQLException {
+        LoginDataDriver ldd = new LoginDataDriver("LoginDataDriverTester.sqlite");
+        ldd.connect();
+    }
     @Test
     void constructor() throws SQLException {
         LoginDataDriver ldd = new LoginDataDriver("LoginDataDriverTester.sqlite");
@@ -24,5 +33,23 @@ public class LoginDataDriverTest {
         ldd.connect();
         ldd.addUser("kevinlam0", "password");
         ldd.commit();
+    }
+    @Test
+    void doesUserExist_true() throws SQLException {
+        LoginDataDriver ldd = new LoginDataDriver("LoginDataDriverTester.sqlite");
+        ldd.connect();
+        assertTrue(ldd.doesUserExist("kevinlam0"));
+    }
+    @Test
+    void doesUserExist_false() throws SQLException {
+        LoginDataDriver ldd = new LoginDataDriver("LoginDataDriverTester.sqlite");
+        ldd.connect();
+        assertFalse(ldd.doesUserExist("kevinlam"));
+    }
+    @Test
+    void doesUserExist_falseCase() throws SQLException {
+        LoginDataDriver ldd = new LoginDataDriver("LoginDataDriverTester.sqlite");
+        ldd.connect();
+        assertFalse(ldd.doesUserExist("Kevinlam"));
     }
 }
