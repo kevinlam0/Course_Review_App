4defb74ab8b16546c28314d840214a6a9450a00d
kevinlam0 <kevinclam02@gmail.com>
2023-11-19 22:00:25 -0500

    tested the creationTable and adding course

--- src/test/java/edu/virginia/sde/hw6/CourseDataDriverTest.java
+++ src/test/java/edu/virginia/sde/hw6/CourseDataDriverTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CourseDataDriverTest {
     @Test
     void connect() throws SQLException {
@@ -31,8 +33,27 @@ public class CourseDataDriverTest {
     void addCourse_longerThan4Char() throws SQLException {
         CourseDataDriver cdd = new CourseDataDriver("LoginDataDriverTester.sqlite");
         cdd.connect();
-        cdd.addCourse("CSSSS", 3140, "Software Development");
-        cdd.commit();
+        assertThrows(SQLException.class, () -> cdd.addCourse("CSSSS", 3140, "Software Development"));
+    }
+    @Test
+    void addCourse_longerThan4CourseNumber() throws SQLException {
+        CourseDataDriver cdd = new CourseDataDriver("LoginDataDriverTester.sqlite");
+        cdd.connect();
+        assertThrows(SQLException.class, () -> cdd.addCourse("CSSS", 10000, "Software Development"));
+    }
+    @Test
+    void addCourse_0CourseNumber() throws SQLException {
+        CourseDataDriver cdd = new CourseDataDriver("LoginDataDriverTester.sqlite");
+        cdd.connect();
+//        cdd.addCourse("CSSS", 0, "Software Development");
+//        cdd.commit();
+    }
+    @Test
+    void addCourse_9999CourseNumber() throws SQLException {
+        CourseDataDriver cdd = new CourseDataDriver("LoginDataDriverTester.sqlite");
+        cdd.connect();
+//        cdd.addCourse("CSSS", 9999, "Software Development");
+//        cdd.commit();
     }
 
 }
