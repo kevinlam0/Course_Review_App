1ef5c957a9705ad7d4cf46b5d00a67cab65c652a
kevinlam0 <kevinclam02@gmail.com>
2023-11-30 23:17:01 -0500

    idk

---
+++ src/main/java/edu/virginia/sde/reviews/UserLogic.java
@@ -0,0 +1,7 @@
+package edu.virginia.sde.reviews;
+
+public class UserLogic {
+    private static String username;
+    public static void setUsername(String user) {username = user;}
+    public static String getUsername() { return username; }
+}
