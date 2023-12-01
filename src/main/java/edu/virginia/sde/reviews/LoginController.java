18331647e1b5b51c96c03d590639f94d54e293bb
kevinlam0 <kevinclam02@gmail.com>
2023-11-30 21:11:29 -0500

    started making create table query

--- src/main/java/edu/virginia/sde/reviews/LoginController.java
+++ src/main/java/edu/virginia/sde/reviews/LoginController.java
@@ -1,18 +1,9 @@
 package edu.virginia.sde.reviews;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
-public class LoginController {
-    public void logInExistingAccount(String username, String password) {
-        // THERE NEEDS TO BE A TABLE FOR THE ACCOUNTS
-
-        /* IF THE USERNAME OR PASSWORD IS INCORRECT DO SOMETHING */
-
-        // USERNAME DOES NOT EXIST
-
-        // PASSWORD IS INCORRECT
-
-    }
+public class LogInController {
     @FXML
     public void handleLogin(){
 
