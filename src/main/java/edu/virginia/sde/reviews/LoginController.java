27e92da89bee277f1bdc8f331ab33c521ff10622
suwanl0620 <suwanl0620@gmail.com>
2023-11-30 23:01:31 -0500

    patch

---
+++ src/main/java/edu/virginia/sde/reviews/LoginController.java
@@ -0,0 +1,26 @@
+package edu.virginia.sde.reviews;
+
+import javafx.fxml.FXML;
+
+public class LoginController {
+    public void logInExistingAccount(String username, String password) {
+        // THERE NEEDS TO BE A TABLE FOR THE ACCOUNTS
+
+        /* IF THE USERNAME OR PASSWORD IS INCORRECT DO SOMETHING */
+
+        // USERNAME DOES NOT EXIST
+
+        // PASSWORD IS INCORRECT
+
+    }
+    @FXML
+    public void handleLogin(){
+
+    }
+    public void handleCreateAccount(){
+
+    }
+    public void handleClose(){
+
+    }
+}
