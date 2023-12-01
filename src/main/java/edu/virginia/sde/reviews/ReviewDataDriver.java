3ee00f9c4f90f37c83fc3c0c8f96529cb1cc74da
chengJazhua <chengjazhua@gmail.com>
2023-11-30 21:54:52 -0500

    Added review database driver + test

--- src/main/java/edu/virginia/sde/reviews/ReviewDataDriver.java
+++ src/main/java/edu/virginia/sde/reviews/ReviewDataDriver.java
@@ -11,14 +11,40 @@ public class ReviewDataDriver extends DatabaseDriver{
                 CREATE TABLE IF NOT EXISTS Reviews
                 (
                     ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Course_ID Integer,
+                    Username Text,
+                    Time DATETIME,
+                    Review TEXT,
+                    Rating INTEGER, 
                     FOREIGN KEY (Course_ID) REFERENCES Courses(ID) ON DELETE CASCADE,
                     FOREIGN KEY (Username) REFERENCES Users(Username) ON DELETE CASCADE,
-                    Review TEXT,
-                    
+                    UNIQUE (Course_ID, Username)
                 );
                 """;
         PreparedStatement statement = connection.prepareStatement(query);
         statement.execute();
         statement.close();
     }
+
+
+    public void addReview(int courseID, String username, int rating, String comment) throws SQLException {
+        try {
+            if (connection.isClosed()) {
+                throw new IllegalStateException("Connection is not open");
+            }
+            PreparedStatement statement = connection.prepareStatement(
+                    "INSERT INTO Reviews(Course_ID, Username, Time, Review, Rating) values (?, ?, CURRENT_TIMESTAMP, ?, ?)");
+            statement.setInt(1, courseID);
+            statement.setString(2, username);
+            statement.setString(3, comment);
+            statement.setInt(4, rating);
+            statement.execute();
+            statement.close();
+        }
+        catch (SQLException e) {
+            super.rollback();
+            throw e;
+        }
+    }
 }
+
