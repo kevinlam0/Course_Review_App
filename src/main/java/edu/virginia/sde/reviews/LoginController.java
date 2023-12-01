a5cce46bbfe0b1fd0a29931a22f0f5e1758fde08
Sammy Kee <slk7ws@virginia.edu>
2023-11-30 22:20:11 -0500

    commit

--- src/main/java/edu/virginia/sde/reviews/LoginController.java
+++ src/main/java/edu/virginia/sde/reviews/LoginController.java
@@ -1,12 +1,18 @@
 package edu.virginia.sde.reviews;
 
-public class LoginController {
-    public void logInExistingAccount(String username, String password) {
-        // THERE NEEDS TO BE A TABLE FOR THE ACCOUNTS
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
-        /* IF THE USERNAME OR PASSWORD IS INCORRECT DO SOMETHING */
-        // USERNAME DOES NOT EXIST
+public class LogInController {
+    @FXML
+    public void handleLogin(){
 
-        // PASSWORD IS INCORRECT
     }
+    public void handleCreateAccount(){
+
+    }
+    public void handleClose(){
+
+    }
+
 }
