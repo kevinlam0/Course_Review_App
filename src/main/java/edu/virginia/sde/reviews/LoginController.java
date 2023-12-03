146f9d75d5f061d795b9f6fb09b33e727ecd6cf6
kevinlam0 <kevinclam02@gmail.com>
2023-12-03 15:37:21 -0500

    try fixing the logincontroller file

--- src/main/java/edu/virginia/sde/reviews/LoginController.java
+++ src/main/java/edu/virginia/sde/reviews/LoginController.java
@@ -1,17 +1,65 @@
 package edu.virginia.sde.reviews;
-
+import edu.virginia.sde.reviews.Exceptions.UserAlreadyExistsException;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
 
-public class LogInController {
-    @FXML
-    public void handleLogin(){
+import java.awt.event.ActionEvent;
+import java.sql.SQLException;
 
+public class LoginController {
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private Label errorLabel;
+    private LoginLogic loginLogic;
+
+    public LoginController(LoginLogic loginLogic){
+        this.loginLogic = loginLogic;
+    }
+    public void handleLogin(){
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        try {
+            if (loginLogic.isLoginSuccessful(username, password)) {
+                System.out.println("Login successful");
+            } else {
+                // handle unsuccessful login
+                errorLabel.setText("Invalid username or password");
+
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+            errorLabel.setText("Database error");
+        }
     }
     public void handleCreateAccount(){
+        String newUsername = usernameField.getText();
+        String newPassword = passwordField.getText();
 
+        try {
+            loginLogic.createUser(newUsername, newPassword);
+            // handle successful user creation
+            System.out.println("User created successfully");
+        } catch (UserAlreadyExistsException e) {
+            // handle user already exists error
+            errorLabel.setText("User already exists");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            // handle database-related errors
+            errorLabel.setText("Database error");
+        }
     }
     public void handleClose(){
-
+        Platform.exit();
+    }
+    public void switchToCourseSearch(ActionEvent event){
+        // implement after making course search fxml
     }
 }
