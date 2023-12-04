e91613467d9899817329eb20928a035f885ac59e
kevinlam0 <kevinclam02@gmail.com>
2023-12-04 13:54:22 -0500

    Added method for editing review

--- src/main/java/edu/virginia/sde/reviews/ReviewLogic.java
+++ src/main/java/edu/virginia/sde/reviews/ReviewLogic.java
@@ -1,9 +1,12 @@
 package edu.virginia.sde.reviews;
 
+import java.sql.SQLException;
+
 public class ReviewLogic {
     private static ReviewDataDriver reviewDataDriver;
-    public static void editReview() {
-        String username = "";
-
+    public static void editReview(int course_id, int rating, String comment) throws SQLException {
+        String username = Credentials.getUsername();
+        reviewDataDriver.deleteReview(course_id, username);
+        reviewDataDriver.addReview(course_id, username, rating, comment);
     }
 }
