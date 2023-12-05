ad4d892dfa3d4a5c73ed682774b171510fad698e
Sammy Kee <slk7ws@virginia.edu>
2023-12-04 23:03:31 -0500

    Fixed initialization method

--- src/main/java/edu/virginia/sde/reviews/MyReviewsController.java
+++ src/main/java/edu/virginia/sde/reviews/MyReviewsController.java
@@ -1,6 +1,9 @@
 package edu.virginia.sde.reviews;
 
+import com.sun.javafx.charts.Legend;
 import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -16,6 +19,8 @@ import javax.xml.stream.events.Comment;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
+
+//import static edu.virginia.sde.reviews.ReviewLogic.reviewDataDriver;
 
 public class MyReviewsController {
     @FXML
@@ -33,12 +38,17 @@ public class MyReviewsController {
 
     @FXML
     private ListView<String> reviewsListView;
+    private ReviewDataDriver reviewDataDriver;
 
+    private ObservableList<Review> reviewsData = FXCollections.observableArrayList();
     @FXML
     private Button backButton;
 
     private LoginLogic loginLogic;
     private Stage primaryStage;
+
+    private  int currentCourseID;
+    private String username;
 
     private void initialize(){
 
@@ -49,10 +59,39 @@ public class MyReviewsController {
             commentsColumn.setCellValueFactory(new PropertyValueFactory<>("comment"));
 
             //add logic to get data from database
+            populateUserReviewsData(username);
 
-
-        //missing try-catch for database errors(?)
     }
+
+    private void populateUserReviewsData(String username){
+        try{
+            reviewsData.clear();
+
+
+
+            reviewsData.addAll(reviewDataDriver.findReviewsByUsername(username, currentCourseID));
+
+            reviewTable.setItems(reviewsData);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    /**
+     * private void handleSearch(){
+     *         String subject = subjectField.getText();
+     *         int number = parseNumber(numberField.getText());
+     *         String title = titleField.getText();
+     *
+     *         try {
+     *             ObservableList<Course> searchResults = FXCollections.observableArrayList(courseLogic.filterCoursesBy(subject, number, title));
+     *             courseTable.setItems(searchResults);
+     *         } catch (SQLException e) {
+     *             e.printStackTrace();
+     *             // handle database errors
+     *         }
+     *     }
+     */
+
 
 
 
