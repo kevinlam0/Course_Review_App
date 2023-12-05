7d49f7b93f7d4fe9a013f7c7c1057bfce958cab7
Sammy Kee <slk7ws@virginia.edu>
2023-12-04 20:52:05 -0500

    Worked on back button for myreviews

--- src/main/java/edu/virginia/sde/reviews/MyReviewsController.java
+++ src/main/java/edu/virginia/sde/reviews/MyReviewsController.java
@@ -2,11 +2,15 @@ package edu.virginia.sde.reviews;
 
 import javafx.beans.Observable;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
 import javax.xml.stream.events.Comment;
 import java.sql.SQLException;
@@ -43,5 +47,26 @@ public class MyReviewsController {
     @FXML
     private void backToCourseSearch(){
         // scene switch CourseSearch scene
+        try {
+            // Load the FXML file for the CourseSearch scene
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("course-search.fxml"));
+            Parent root = loader.load();
+
+            // Create a new Scene with the loaded FXML content
+            Scene scene = new Scene(root);
+
+            // Get the Stage from the current button (assuming the button is part of a Scene)
+            Stage stage = (Stage) backButton.getScene().getWindow();
+
+            // Set the new Scene on the Stage
+            stage.setScene(scene);
+
+
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-}
+    }
+
+
