ca63833260b42c0891e4a81908356e3cc7310979
Sammy Kee <slk7ws@virginia.edu>
2023-12-04 23:03:16 -0500

    Added method to filter reviews by username

--- src/main/java/edu/virginia/sde/reviews/ReviewDataDriver.java
+++ src/main/java/edu/virginia/sde/reviews/ReviewDataDriver.java
@@ -29,7 +29,27 @@ public class ReviewDataDriver extends DatabaseDriver{
         this.commit();
     }
 
+    public ArrayList<Review> findReviewsByUsername(String username, int courseID) throws SQLException{
+        PreparedStatement statement = connection.prepareStatement("SELECT * FROM Reviews WHERE Username = ? AND Course_ID = ?");
+        statement.setString(1, username);
+        statement.setInt(2, courseID);
+        ResultSet results = statement.executeQuery();
 
+        ArrayList<Review> reviews = new ArrayList<>();
+        while(results.next()){
+            Review review = new Review(
+                    results.getInt(2),
+                    results.getString(3),
+                    results.getString(4),
+                    results.getString(5),
+                    results.getInt(6)
+            );
+            reviews.add(review);
+        }
+        statement.close();
+        return reviews;
+
+    }
     public void addReview(int courseID, String username, int rating, String comment) throws SQLException {
         try {
             if (connection.isClosed()) {
