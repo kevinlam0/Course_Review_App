df3940fa56f9df0afc89b004770be32394213383
kevinlam0 <kevinclam02@gmail.com>
2023-12-31 19:16:29 -0500

    reset start up to login screen

--- src/main/java/edu/virginia/sde/reviews/CourseReviewsApplication.java
+++ src/main/java/edu/virginia/sde/reviews/CourseReviewsApplication.java
@@ -25,27 +25,18 @@ public class CourseReviewsApplication extends Application {
 
         createTables();
         recalculateRatingAverageForAllCourses();
+
         /* MAIN START SCREEN */
         LoginLogic.setLoginDataDriver(new LoginDataDriver(Credentials.getSqliteDataName()));
-
-//        FXMLLoader fxmlLoader = openScene(primaryStage, "log-in.fxml", "Course Reviews");
-//        LoginController controller = (LoginController) fxmlLoader.getController();
-//        controller.setPrimaryStage(primaryStage);
-        ReviewLogic.setReviewDataDriver(new ReviewDataDriver(Credentials.getSqliteDataName()));
-        CourseLogic.setCourseDataDriver(new CourseDataDriver(Credentials.getSqliteDataName()));
-        CourseLogic.setReviewDataDriver(new ReviewDataDriver(Credentials.getSqliteDataName()));
-        CourseLogic.setCurrentCourse(2);
-        FXMLLoader fxmlLoader = CourseReviewsApplication.openScene(primaryStage,"course-reviews.fxml", "Review of Course");
-        CourseReviewsController controller = (CourseReviewsController) fxmlLoader.getController();
+        FXMLLoader fxmlLoader = openScene(primaryStage, "log-in.fxml", "Course Reviews");
+        LoginController controller = fxmlLoader.getController();
         controller.setPrimaryStage(primaryStage);
 
-        // Shows up on top
+        /* Show window on top */
         primaryStage.setAlwaysOnTop(true);
-        // Turns off the show on top
-//        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
-//            primaryStage.setAlwaysOnTop(false);
-//        }));
-//        timeline.play();
+        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
+            primaryStage.setAlwaysOnTop(false); }));
+        timeline.play();
     }
 
     private static void createTables() {
